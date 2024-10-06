ec38ade0185b27081cd4a7fef3401da99c649719
stepan.rutz <stepan.rutz@gmx.de>
2024-10-06 12:48:01 +0200

    nuxterrorboundary

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,17 @@
 <template>
     <div class="grow h-1 flex flex-col items-center justify-center overflow-hidden">
-        <div>Hello World</div>
+        <NuxtErrorBoundary @error="logError">
+            <SequenceBanner title="React" />
+            <template #error="errorhandle">
+                <div class="text-2xl md:text-4xl lg:text-8xl uppercase">
+                    <div>Your banner was censored</div>
+                </div>
+            </template>
+        </NuxtErrorBoundary>
     </div>
 </template>
+<script setup lang="ts">
+const logError = (error: any) => {
+    console.error(error)
+}
+</script>
