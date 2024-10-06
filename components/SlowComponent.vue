76d893677cb72cabca1b3e9360058d2ec5958830
stepan.rutz <stepan.rutz@gmx.de>
2024-10-06 12:16:05 +0200

    Suspended slow child added

---
+++ components/SlowComponent.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="lex flex-col items-center justify-center overflow-hidden">
+        <div>I have arrived</div>
+    </div>
+</template>
+<script setup lang="ts">
+// wait 10 seconds
+await new Promise(resolve => setTimeout(resolve, 10_000))
+</script>
